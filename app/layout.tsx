3d8d3d1ffe6f801aa6f00418e701dfd6cc739d38
Awogbami Tunde <oladipupo.jayden@gmail.com>
2024-12-08 17:48:47 +0100

    css Add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 import { Inter } from 'next/font/google'
-import "./globals.css"
+import '@/styles/globals.css'
 import { Header } from "@/components/header"
 import { Footer } from "@/components/footer"
 
@@ -17,13 +17,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="min-h-screen">
+    <html lang="en">
       <body className={inter.className}>
-        <div className="relative flex min-h-screen flex-col">
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </div>
+        <Header />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
